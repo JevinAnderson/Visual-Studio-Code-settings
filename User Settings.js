0c178b86176620d34774234b5ce880846587ba64
Jevin Anderson <jevin.anderson@recruitingventures.com>
2016-09-01 10:51:06 -0700

    Create User Settings.js

---
+++ User Settings.js
@@ -0,0 +1,14 @@
+// Place your settings in this file to overwrite the default settings
+{
+  "editor.tabSize": 2,
+  "files.trimTrailingWhitespace": true,
+  "files.autoSave": "afterDelay",
+  "editor.formatOnType": true,
+  "explorer.workingFiles.maxVisible": 15,
+  "bookmarks.navigateThroughAllFiles": true,
+  "bookmarks.saveBookmarksBetweenSessions": true,
+  "javascript.format.insertSpaceAfterFunctionKeywordForAnonymousFunctions": false,
+  "files.associations": {
+    "*.asp": "vb"
+  }
+}
